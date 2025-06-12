99f26f136c979ffc4d715e540bf1b44919f9cac7
jcbcnielsen <jcbcnielsen@proton.me>
2025-06-11 21:24:42 -0400

    Week 6 Edit A

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>([
+            new Square("red", 5),
+            new Rectangle("green", 2, 3),
+            new Circle("blue", 4)
+        ]);
+
+        foreach (Shape s in shapes)
+        {
+            Console.WriteLine($"Color: {s.GetColor()}, Area: {s.GetArea()}");
+        }
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,20 @@
+public abstract class Shape
+{
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+    public void SetColor(string newColor)
+    {
+        _color = newColor;
+    }
+
+    public abstract double GetArea();
+}
